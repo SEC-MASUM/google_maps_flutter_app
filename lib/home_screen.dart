b278661b0a37c9844d8c1c2e930ba9df4bcffda7
SEC-MASUM <masum.jess42@gmail.com>
2024-12-17 00:03:25 +0600

    add markers and circles

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -32,6 +32,58 @@ class _HomeScreenState extends State<HomeScreen> {
           googleMapController = controller;
         },
         trafficEnabled: true,
+        markers: <Marker>{
+          Marker(
+            markerId: const MarkerId("initial-position"),
+            position: const LatLng(23.18618077214386, 89.50080140316282),
+            infoWindow: InfoWindow(
+              title: "Home",
+              onTap: () {
+                print("On tapped home");
+              },
+            ),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueOrange),
+            draggable: true,
+          ),
+          Marker(
+            markerId: const MarkerId("home"),
+            position: const LatLng(23.185840604818065, 89.50092673301697),
+            draggable: true,
+            onDragStart: (LatLng onStartLatLng) {
+              print("On start drag $onStartLatLng");
+            },
+            onDragEnd: (LatLng onEndLatLng) {
+              print("On end drag $onEndLatLng");
+            },
+          ),
+        },
+        circles: <Circle>{
+          Circle(
+            circleId: const CircleId("initial-circle"),
+            fillColor: Colors.red.withOpacity(.3),
+            center: const LatLng(23.185909949072826, 89.50093243271112),
+            radius: 30,
+            strokeColor: Colors.blue,
+            strokeWidth: 5,
+            visible: true,
+            onTap: () {
+              print("Entered into Novo Nest zone");
+            },
+          ),
+          Circle(
+            circleId: const CircleId("land-circle"),
+            fillColor: Colors.blue.withOpacity(.3),
+            center: const LatLng(23.185561686452512, 89.50120065361261),
+            radius: 50,
+            strokeColor: Colors.blue,
+            strokeWidth: 5,
+            visible: true,
+            onTap: () {
+              print("Entered into Novo Nest zone");
+            },
+          ),
+        },
       ),
     );
   }
