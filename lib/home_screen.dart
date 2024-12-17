11f65ed5de405f792aeda7f945ec191fdd64d78e
SEC-MASUM <masum.jess42@gmail.com>
2024-12-17 20:53:01 +0600

    add Polyline, Polygon, animateCamera(CameraUpdate.newCameraPosition(CameraPosition()))

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -28,6 +28,8 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         zoomControlsEnabled: true,
         zoomGesturesEnabled: true,
+        compassEnabled: true,
+        // scrollGesturesEnabled: false,
         onMapCreated: (GoogleMapController controller) {
           googleMapController = controller;
         },
@@ -84,6 +86,47 @@ class _HomeScreenState extends State<HomeScreen> {
             },
           ),
         },
+        polylines: <Polyline>{
+          const Polyline(
+              polylineId: PolylineId("initial-polyline"),
+              color: Colors.amber,
+              width: 3,
+              jointType: JointType.round,
+              points: <LatLng>[
+                LatLng(23.18406383865313, 89.50221452862024),
+                LatLng(23.18439330496788, 89.50218368321657),
+                LatLng(23.185597745544666, 89.50261451303959),
+                LatLng(23.185781431025568, 89.50206633657217)
+              ])
+        },
+        polygons: <Polygon>{
+          Polygon(
+            polygonId: const PolygonId("initial-polygon"),
+            fillColor: Colors.purple.withOpacity(0.4),
+            strokeColor: Colors.lightBlue,
+            strokeWidth: 2,
+            points: const <LatLng>[
+              LatLng(23.17575448543142, 89.49260249733925),
+              LatLng(23.171762668683115, 89.50581774115562),
+              LatLng(23.180361678689593, 89.50727082788944),
+              LatLng(23.184192050149875, 89.49105989187956),
+            ],
+          ),
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          googleMapController.animateCamera(
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                zoom: 16,
+                target: LatLng(23.18618077214386, 89.50080140316282),
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.location_on),
       ),
     );
   }
